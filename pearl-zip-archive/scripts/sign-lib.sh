3acc5b0d8ec3161774d648227bab6ce8b0045206
Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>
2021-08-01 22:53:31 +0100

    PZ-135 | Jar Signing scripts: ArchiveService,Zip4J
    + Jar signing implemented
    + Publish maven package to personal remote repository also implemented
    
    Signed-off-by: Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>

---
+++ pearl-zip-archive/scripts/sign-lib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright © 2021 92AK
+#
+
+# Parameters
+# $1 - Release e.g. PA-0.0.0.1
+#
+
+# Populate environment variables
+P_RELEASE=$1
+RELEASE=$1
+echo "JAVA_HOME: $JAVA_HOME"
+
+echo 'Configuring release name...'
+P_RELEASE="${P_PREFIX_RELEASE:+$P_PREFIX_RELEASE-}$P_RELEASE"
+echo "Release name has been set to: ${P_RELEASE}"
+
+# 1. Sign jar using keystore
+echo "Signing plugin archive..."
+ARCHIVE=$(ls target/pearl-zip-archive-${RELEASE}.jar)
+echo "Archive to sign: $ARCHIVE"
+echo $(cat /opt/.store/.pw-signer) | ${JAVA_HOME}/bin/jarsigner -tsa https://freetsa.org/tsr -keystore /opt/.store/.ks-signer -storepass $(cat /opt/.store/.pw) "$ARCHIVE" 92ak
