66cb7d32aaaaeafa9385ccd855db7fbb40972b64
Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>
2021-09-20 21:54:05 +0100

    PZ-164 | Java OpenJDK 17 LTS Migration (PearlZip and Zip4j Plugin)
    + Upgraded PearlZip and Zip4J JDK requirements to JDK 17 LTS
    + Updated modularised dependencies to be compiled with JDK 17
    + Removed preview switch from compilation
    + Updated build scripts to utilise and check for JDK 17 in environment
    
    Signed-off-by: Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>

--- pearl-zip-assembly-osx/src/main/bash/gen.sh
+++ pearl-zip-assembly-osx/src/main/bash/gen.sh
@@ -6,6 +6,6 @@ rm -rf work
 rm -rf out
 mkdir work out
 
-jdeps --ignore-missing-deps --multi-release 15 --generate-module-info work $1
-cp "work/$(ls -1 work)/versions/15/module-info.java" $(pwd)/work/$(ls -1 work)/module-info.java
+jdeps --ignore-missing-deps --multi-release 17 --generate-module-info work $1
+cp "work/$(ls -1 work)/versions/17/module-info.java" $(pwd)/work/$(ls -1 work)/module-info.java
 ls -l work
