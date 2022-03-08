4f11de2236c00d5a1882c60fa83fb47c6216fd56
Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>
2022-03-08 08:59:12 +0000

    PZ-225 | Fix start up race condition issue when loading the system menu bar
    + Generation of System menu is done with a pre-check to ensure it is done so on the JFX thread
    
    Signed-off-by: Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>

--- pearl-zip-ui/src/main/java/com/ntak/pearlzip/ui/pub/SysMenuController.java
+++ pearl-zip-ui/src/main/java/com/ntak/pearlzip/ui/pub/SysMenuController.java
@@ -122,41 +122,40 @@ public class SysMenuController {
 
         ArchiveUtil.refreshRecentFileMenu(mnuOpenRecent);
 
-        mnuOptions.setOnAction((e)->{
-            try {
-                Stage newStage = new Stage();
-                AnchorPane root;
+        mnuOptions.setOnAction((e)-> JFXUtil.runLater(() -> {
+        try {
+            Stage newStage = new Stage();
+            AnchorPane root;
 
-                newStage.setTitle(resolveTextKey(TITLE_OPTIONS_PATTERN));
-                newStage.setResizable(false);
-                FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(ZipLauncher.class.getClassLoader()
-                                                    .getResource("frmOptions.fxml"));
-                loader.setResources(LOG_BUNDLE);
-                root = loader.load();
+            newStage.setTitle(resolveTextKey(TITLE_OPTIONS_PATTERN));
+            newStage.setResizable(false);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ZipLauncher.class.getClassLoader()
+                                                .getResource("frmOptions.fxml"));
+            loader.setResources(LOG_BUNDLE);
+            root = loader.load();
 
-                Scene scene = new Scene(root);
-                newStage.setScene(scene);
-                FrmOptionsController controller = loader.getController();
-                controller.initData(newStage);
+            Scene scene = new Scene(root);
+            newStage.setScene(scene);
+            FrmOptionsController controller = loader.getController();
+            controller.initData(newStage);
 
-                newStage.show();
-                newStage.setAlwaysOnTop(true);
-            } catch (Exception exc) {
-                // LOG: Issue creating stage.\nException type: %s\nMessage:%s\nStack trace:\n%s
-                LOGGER.warn(resolveTextKey(LOG_ISSUE_CREATING_STAGE, exc.getClass().getCanonicalName(),
-                                           exc.getMessage(),
-                                           LoggingUtil.getStackTraceFromException(exc)));
-                // TITLE: ERROR: Issue creating stage
-                // HEADER: There was an issue creating the required dialog
-                // BODY: Upon initiating function '%s', an issue occurred on attempting to create the dialog. This
-                // function will not proceed any further.
-                raiseAlert(Alert.AlertType.ERROR, resolveTextKey(TITLE_ISSUE_CREATING_STAGE),
-                           resolveTextKey(HEADER_ISSUE_CREATING_STAGE),
-                           resolveTextKey(BODY_ISSUE_CREATING_STAGE, this.getClass().getName()), exc,
-                           null);
-            }
-        });
+            newStage.show();
+            newStage.setAlwaysOnTop(true);
+        } catch (Exception exc) {
+            // LOG: Issue creating stage.\nException type: %s\nMessage:%s\nStack trace:\n%s
+            LOGGER.warn(resolveTextKey(LOG_ISSUE_CREATING_STAGE, exc.getClass().getCanonicalName(),
+                                       exc.getMessage(),
+                                       LoggingUtil.getStackTraceFromException(exc)));
+            // TITLE: ERROR: Issue creating stage
+            // HEADER: There was an issue creating the required dialog
+            // BODY: Upon initiating function '%s', an issue occurred on attempting to create the dialog. This
+            // function will not proceed any further.
+            raiseAlert(Alert.AlertType.ERROR, resolveTextKey(TITLE_ISSUE_CREATING_STAGE),
+                       resolveTextKey(HEADER_ISSUE_CREATING_STAGE),
+                       resolveTextKey(BODY_ISSUE_CREATING_STAGE, this.getClass().getName()), exc,
+                       null);
+        }}));
 
         mnuNotifications.setOnAction((e)-> JFXUtil.runLater(JFXUtil::showNotifications));
         mnuNewVersion.setOnAction((e)->checkNewVersionAvailable());
