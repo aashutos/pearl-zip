e0d37d267212ff290aeab2d47b4b6db62a084ad9
Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>
2023-03-19 22:03:32 +0000

    PZ-172 | Modularising test code better in terms of preparation and check steps
    + Updates to FileInfoTestFX
    + Minor TestFX utility method updates
    
    Signed-off-by: Aashutos Kakshepati <aashutos_kakshepati@hotmail.com>

--- pearl-zip-ui/src/test/java/com/ntak/pearlzip/ui/util/PearlZipSpecifications.java
+++ pearl-zip-ui/src/test/java/com/ntak/pearlzip/ui/util/PearlZipSpecifications.java
@@ -12,12 +12,15 @@ import com.ntak.testfx.TestFXConstants;
 import com.ntak.testfx.specifications.CommonSpecifications;
 import javafx.geometry.Point2D;
 import javafx.scene.control.*;
+import javafx.util.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.testfx.api.FxRobot;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Objects;
 
 import static com.ntak.pearlzip.ui.constants.ResourceConstants.SSV;
@@ -277,9 +280,71 @@ public class PearlZipSpecifications {
         simExtractFile(robot, targetLocation);
     }
 
-    public static void whenEntrySelectedInCurrentWindow(FxRobot robot, String entryName) {
+    public static TableRow<FileInfo> whenEntrySelectedInCurrentWindow(FxRobot robot, String entryName) {
         TableView<FileInfo> fileContentsView = robot.lookup("#fileContentsView").queryAs(TableView.class);
-        FormUtil.selectTableViewEntry(robot, fileContentsView, FileInfo::getFileName,
+        return FormUtil.selectTableViewEntry(robot, fileContentsView, FileInfo::getFileName,
                                       entryName).get();
+    }
+
+    public static void thenFileInfoScreenContentsMatchesFileMetaData(FxRobot robot, FileInfo fileInfo) {
+        Label lblIndexValue = robot.lookup("#lblIndexValue").queryAs(Label.class);
+        Label lblLevelValue = robot.lookup("#lblLevelValue").queryAs(Label.class);
+        Label lblFilenameValue = robot.lookup("#lblFilenameValue").queryAs(Label.class);
+        Label lblHashValue = robot.lookup("#lblHashValue").queryAs(Label.class);
+        Label lblRawSizeValue = robot.lookup("#lblRawSizeValue").queryAs(Label.class);
+        Label lblPackedSizeValue = robot.lookup("#lblPackedSizeValue").queryAs(Label.class);
+        Label lblFolderValue = robot.lookup("#lblFolderValue").queryAs(Label.class);
+        Label lblEncryptValue = robot.lookup("#lblEncryptValue").queryAs(Label.class);
+        Label lblCommentsValue = robot.lookup("#lblCommentsValue").queryAs(Label.class);
+        Label lblLastWriteTimeValue = robot.lookup("#lblLastWriteTimeValue").queryAs(Label.class);
+        Label lblLastAccessTimeValue = robot.lookup("#lblLastAccessTimeValue").queryAs(Label.class);
+        Label lblCreateTimeValue = robot.lookup("#lblCreateTimeValue").queryAs(Label.class);
+        Label lblUserValue = robot.lookup("#lblUserValue").queryAs(Label.class);
+        Label lblGroupValue = robot.lookup("#lblGroupValue").queryAs(Label.class);
+
+        robot.clickOn("#tpGeneral");
+        robot.sleep(5, MILLISECONDS);
+        Assertions.assertEquals(String.valueOf(fileInfo.getIndex()), lblIndexValue.getText(), "Index does not match");
+        Assertions.assertEquals(String.valueOf(fileInfo.getLevel()), lblLevelValue.getText(), "Level does not match");
+        Assertions.assertEquals(String.valueOf(fileInfo.getFileName()), lblFilenameValue.getText(),
+                                "Filename does not match");
+        Assertions.assertEquals(String.format("0x%s",Long.toHexString(fileInfo.getCrcHash()).toUpperCase()), lblHashValue.getText(),
+                                "Hash does not match");
+        Assertions.assertEquals(String.valueOf(fileInfo.getRawSize()), lblRawSizeValue.getText(),
+                                "Raw size does not match");
+        Assertions.assertEquals(String.valueOf(fileInfo.getPackedSize()), lblPackedSizeValue.getText(),
+                                "Packed size does not match");
+        Assertions.assertEquals(fileInfo.isFolder()?"folder":"file", lblFolderValue.getText(),
+                                "Is Folder does not match");
+        Assertions.assertEquals(fileInfo.isEncrypted()?"encrypted":"plaintext", lblEncryptValue.getText(),
+                                "Is Encrypted does not match");
+        Assertions.assertEquals(fileInfo.getComments(), lblCommentsValue.getText(),
+                                "Comments does not match");
+
+        robot.clickOn("#tpTimestamps");
+        robot.sleep(5, MILLISECONDS);
+        Assertions.assertEquals(Objects.isNull(fileInfo.getLastWriteTime())?"-":fileInfo.getLastWriteTime()
+                                                                                        .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), lblLastWriteTimeValue.getText(),
+                                "Last Write Timestamp does not match");
+        Assertions.assertEquals(Objects.isNull(fileInfo.getLastAccessTime())?"-":fileInfo.getLastAccessTime()
+                                                                                        .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), lblLastAccessTimeValue.getText(),
+                                "Last Access Timestamp does not match");
+        Assertions.assertEquals(Objects.isNull(fileInfo.getCreationTime())?"-":fileInfo.getCreationTime()
+                                                                                         .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), lblCreateTimeValue.getText(),
+                                "Creation Timestamp does not match");
+
+        robot.clickOn("#tpOwnership");
+        robot.sleep(5, MILLISECONDS);
+        Assertions.assertEquals(fileInfo.getUser(), lblUserValue.getText(), "User does not match");
+        Assertions.assertEquals(fileInfo.getGroup(), lblGroupValue.getText(), "Group does not match");
+
+        robot.clickOn("#tpOther");
+        robot.sleep(5, MILLISECONDS);
+        TableView<Pair<String,String>> props = robot.lookup("#tblOtherInfo").queryAs(TableView.class);
+        List<Pair<String,String>> propList = props.getItems();
+        propList.stream().forEach(p->Assertions.assertEquals(fileInfo.getAdditionalInfoMap().get(p.getKey()),
+                                                             p.getValue(),
+                                                             String.format("Property (%s,%s) did not match",
+                                                                           p.getKey(), p.getValue())));
     }
 }
